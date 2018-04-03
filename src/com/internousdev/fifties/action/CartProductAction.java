54e9cd5a6014fdf35562530e7dfbae0cddc2c4c3
yamacha <yamacha441@gmail.com>
2018-04-03 13:15:32 +0900

    push test

--- src/com/internousdev/fifties/action/CartProductAction.java
+++ src/com/internousdev/fifties/action/CartProductAction.java
@@ -1,5 +1,20 @@
 package com.internousdev.fifties.action;
 
-public class CartProductAction {
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.internousdev.fifties.dto.CartInfoDTO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class CartProductAction extends ActionSupport {
+	private Map<String, Object> session;
+
+	private ArrayList<CartInfoDTO> cartList = new ArrayList<CartInfoDTO>();
+
+
+	public String execute() {
+		return SUCCESS;
+	}
+
 
 }
--- src/com/internousdev/fifties/dto/CartInfoDTO.java
+++ src/com/internousdev/fifties/dto/CartInfoDTO.java
@@ -2,4 +2,22 @@ package com.internousdev.fifties.dto;
 
 public class CartInfoDTO {
 
+	private int id;
+	private String userId;
+	private String tempUserId;
+	private String productName;
+	private String productNameKana;
+	private String imageFilePath;
+	private String releaseCompany;
+	private String releaseDate;
+	private int productId;
+	private int productCount;
+	private int stock;
+	private String itemStock;
+	private int price;
+//	private Date insertDate;
+//	private Date updateDate;
+
+	private String productDescription;
+
 }
