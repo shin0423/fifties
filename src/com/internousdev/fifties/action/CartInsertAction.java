4b8a67c5b5e207977026a8342929a3daeb7ac9c8
yamacha <yamacha441@gmail.com>
2018-04-04 16:40:04 +0900

    yamamoto

--- src/com/internousdev/fifties/action/CartInsertAction.java
+++ src/com/internousdev/fifties/action/CartInsertAction.java
@@ -1,5 +1,12 @@
 package com.internousdev.fifties.action;
 
-public class CartInsertAction {
+import org.apache.struts2.interceptor.SessionAware;
 
+import com.opensymphony.xwork2.ActionSupport;
+
+public class CartInsertAction extends ActionSupport implements SessionAware {
+
+	private Map<String, Object> session;
+	private productCount;
+	private productId;
 }
