8593399de5c6ff679754d5c2a3128974eacfb7a7
tatsuya-0 <36288890+tatsuya-0@users.noreply.github.com>
2018-04-04 21:07:49 +0900

    管理者更新（藤澤）

--- src/com/internousdev/fifties/action/MasterAction.java
+++ src/com/internousdev/fifties/action/MasterAction.java
@@ -1,28 +1,23 @@
 package com.internousdev.fifties.action;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Map;
 
-import com.internousdev.fifties.dao.MasterDAO;
-import com.internousdev.fifties.dto.ProductDTO;
+import org.apache.struts2.interceptor.SessionAware;
 
-public class MasterAction {
-	public Map<String, Object> session;
-	private MasterDAO masterDAO=new MasterDAO();
-	private ArrayList<ProductDTO> ProductList = new ArrayList<ProductDTO>();
+import com.opensymphony.xwork2.ActionSupport;
 
+public class MasterAction extends ActionSupport implements SessionAware{
+	public Map<String,Object>session;
 
-
-	public String execute() throws SQLException {
-
-		String result = SUCCESS;
-		result = SUCCESS;
-
-
-
-
-
+	public String execute(){
+		return SUCCESS;
+	}
+	public Map<String,Object>getSession(){
+		return this.session;
+	}
+	@Override
+	public void setSession(Map<String,Object>session){
+		this.session=session;
 	}
 
 }
