a93fc05ff2d32a5bb90107faa7739abc850f46ed
aptx4869 <siza3737@yahoo.co.jp>
2018-04-04 16:28:27 +0900

    ProductDTO,SearchDAOの更新(中嶋)

--- src/com/internousdev/fifties/dao/ProductSearchDAO.java
+++ src/com/internousdev/fifties/dao/ProductSearchDAO.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.internousdev.fifties.dto.ProductSearchDTO;
+import com.internousdev.fifties.dto.ProductDTO;
 import com.internousdev.fifties.util.DBConnector;
 
 public class ProductSearchDAO {
 
-	public List<ProductSearchDTO> getProduct_Info(String productName, int categoryId) throws SQLException {
-		List<ProductSearchDTO> searchDTOList = new ArrayList<ProductSearchDTO>();
+	public List<ProductDTO> getProduct_Info(String productName, int categoryId) throws SQLException {
+		List<ProductDTO> searchDTOList = new ArrayList<ProductDTO>();
 		DBConnector dbConnector = new DBConnector();
 		Connection con = dbConnector.getConnection();
 		String sql = "SELECT*FROM user_info";
@@ -23,25 +23,41 @@ public class ProductSearchDAO {
 		} else {
 			sql = sql + " WEHRE category_id LIKE '%category%' AND product_name LIKE '%searchWord%'";
 		}
-
 		try {
 			PreparedStatement stmt = con.prepareStatement(sql);
 
 			ResultSet rs = stmt.executeQuery();
 
+			while (rs.next()) {
+				ProductDTO dto = new ProductDTO();
+				dto.setId(rs.getInt("id"));
+				dto.setProduct_id(rs.getInt("product_id"));
+				dto.setProduct_name(rs.getString("product_name"));
+				dto.setProduct_name_kana(rs.getString("product_name_kana"));
+				dto.setProduct_description(rs.getString("product_description"));
+				dto.setCategory_id(rs.getInt("category_id"));
+				dto.setProduct_stock(rs.getInt("product_stock"));
+				dto.setPrice(rs.getInt("price"));
+				dto.setImage_file_path(rs.getString("image_file_path"));
+				dto.setImage_file_name(rs.getString("image_file_name"));
+				dto.setRelease_date(rs.getString("release_date"));
+				dto.setRelease_company(rs.getString("release_company"));
+				dto.setStatus(rs.getInt("status"));
+				dto.setRegist_date(rs.getString("regist_date"));
+				dto.setUpdate_date(rs.getString("update_date"));
+				searchDTOList.add(dto);
+
+			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-
 		} catch (Exception e) {
 			e.printStackTrace();
-
 		} finally {
 			con.close();
-
 		}
-
 		return searchDTOList;
-
 
 	}
 }
+/* category_id LIKE '%category%' AND product_name LIKE '%searchWord%' */
--- src/com/internousdev/fifties/dto/ProductDTO.java
+++ src/com/internousdev/fifties/dto/ProductDTO.java
@@ -17,97 +17,124 @@ public class ProductDTO {
 	private String regist_date;
 	private String update_date;
 
-
-
-	public int getId(){
+	public int getId() {
 		return id;
 	}
-	public void setId(int id){
-		this.id=id;
+
+	public void setId(int id) {
+		this.id = id;
 	}
-	public int getProduct_id(){
+
+	public int getProduct_id() {
 		return product_id;
 	}
-	public void setProduct_id(int product_id){
-		this.product_id=product_id;
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
 	}
-	public String getProduct_name(){
+
+	public String getProduct_name() {
 		return product_name;
 	}
-	public void setProduct_name(String product_name){
-		this.product_name=product_name;
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
 	}
-	public String getProduct_name_kana(){
+
+	public String getProduct_name_kana() {
 		return product_name_kana;
 	}
-	public void setProduct_name_kana(String product_name_kana){
-		this.product_name_kana=product_name_kana;
+
+	public void setProduct_name_kana(String product_name_kana) {
+		this.product_name_kana = product_name_kana;
 	}
-	public String getProduct_description(){
+
+	public String getProduct_description() {
 		return product_description;
 	}
-	public void setProduct_description(String product_description){
-		this.product_description=product_description;
+
+	public void setProduct_description(String product_description) {
+		this.product_description = product_description;
 	}
-	public int getCategory_id(){
+
+	public int getCategory_id() {
 		return category_id;
 	}
-	public void setCategory_id(int category_id){
-		this.category_id=category_id;
+
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
 	}
-	public int getProduct_stock(){
+
+	public int getProduct_stock() {
 		return product_stock;
 	}
-	public void setProduct_stock(int product_stock){
-		this.product_stock=product_stock;
+
+	public void setProduct_stock(int product_stock) {
+		this.product_stock = product_stock;
 	}
-	public int getPrice(){
+
+	public int getPrice() {
 		return price;
 	}
-	public void setPrice(int price){
-		this.price=price;
+
+	public void setPrice(int price) {
+		this.price = price;
 	}
-	public String getImage_file_path(){
+
+	public String getImage_file_path() {
 		return image_file_path;
 	}
-	public void getImage_file_path(String image_file_path){
-		this.image_file_path=image_file_path;
+
+	public void setImage_file_path(String image_file_path) {
+		this.image_file_path = image_file_path;
 	}
-	public String getImage_file_name(){
+
+	public String getImage_file_name() {
 		return image_file_name;
 	}
-	public void setImage_file_name(String image_file_name){
-		this.image_file_name=image_file_name;
+
+	public void setImage_file_name(String image_file_name) {
+		this.image_file_name = image_file_name;
 	}
-	public String getRelease_date(){
+
+	public String getRelease_date() {
 		return release_date;
 	}
-	public void setRelease_date(String release_date){
-		this.release_date=release_date;
+
+	public void setRelease_date(String release_date) {
+		this.release_date = release_date;
 	}
-	public String getRelease_company(){
+
+	public String getRelease_company() {
 		return release_company;
 	}
-	public void setRelease_company(String release_company){
-		this.release_company=release_company;
+
+	public void setRelease_company(String release_company) {
+		this.release_company = release_company;
 	}
-	public int getStatus(){
+
+	public int getStatus() {
 		return status;
 	}
-	public void setStatus(int status){
-		this.status=status;
+
+	public void setStatus(int status) {
+		this.status = status;
 	}
-	public String getRegist_date(){
+
+	public String getRegist_date() {
 		return regist_date;
 	}
-	public void setRegist_date(String regist_date){
-		this.regist_date=regist_date;
+
+	public void setRegist_date(String regist_date) {
+		this.regist_date = regist_date;
 	}
-	public String getUpdate_date(){
+
+	public String getUpdate_date() {
 		return update_date;
 	}
-	public void setUpdate_date(String update_date){
-		this.update_date=update_date;
+
+	public void setUpdate_date(String update_date) {
+		this.update_date = update_date;
 	}
 
 }
